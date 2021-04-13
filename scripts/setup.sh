a35391864d3769a3d73fa81d9ae90df7a963a449
Christian Mahnke <cmahnke@gmail.com>
2021-04-13 15:34:42 +0200

    use more features of base theme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
 echo "Set SKIP_IIIF to something to disable generation of IIIF derivates"
-
 if [[ -z "$SKIP_IIIF" ]] ; then
     ./scripts/iiif.sh
 fi
 
-# Favicons
-# See https://gist.github.com/pfig/1808188
-convert "Source Files/Favicon/Favicon.psd[2]" -background 'rgba(255, 255, 0, .5)' -resize 300x300 -gravity center -extent 300x300  static/images/favicon.png
-convert static/images/favicon.png -resize 256x256 static/images/favicon-256.png
-convert static/images/favicon.png -resize 128x128 static/images/favicon-128.png
-convert static/images/favicon-128.png -resize 16x16 static/images/favicon-16.png
-convert static/images/favicon-128.png -resize 32x32 static/images/favicon-32.png
-convert static/images/favicon-128.png -resize 64x64 static/images/favicon-64.png
-convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
+echo "Calling theme scripts"
+for SCRIPT in $PWD/themes/projektemacher-base/scripts/init/*.sh ; do
+    echo "Running $SCRIPT"
+    bash "$SCRIPT"
+done
 
-yarn install
+# Favicons
+SOURCE="Source Files/Favicon/Favicon.psd[1]" OPTIONS="-background 'rgba(255, 255, 0, .5)' -resize 300x300 -gravity center -extent 300x300" ./themes/projektemacher-base/scripts/favicon.sh

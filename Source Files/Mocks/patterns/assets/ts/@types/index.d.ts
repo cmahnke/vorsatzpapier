5ce18cb26262ea83a03b8dd8a31c7e6eaa0a0888
Christian Mahnke <cmahnke@gmail.com>
2025-05-02 13:29:21 +0200

    Updated Hugo

--- Source Files/Mocks/patterns/assets/ts/@types/index.d.ts
+++ Source Files/Mocks/patterns/assets/ts/@types/index.d.ts
@@ -2,3 +2,8 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*/translations.json" {
+  const content: Record<string,Record<string, Record<string, string>>>;
+  export default content;
+}

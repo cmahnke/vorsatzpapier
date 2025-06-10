5a73c867a4ca111e7fe4dda614a77fae6e74817a
Christian Mahnke <cmahnke@gmail.com>
2025-06-10 22:40:06 +0200

    Try to use generator build script

--- Source Files/Mocks/scripts/lucienne.sh
+++ Source Files/Mocks/scripts/lucienne.sh
@@ -3,5 +3,7 @@
 npm i
 npm run bundle
 
+mkdir -p ../../assets/js/lucienne/ ../../assets/scss/lucienne/
+
 cp build/dist/lucienne-0.1.0.js ../../assets/js/lucienne/
 cp build/dist/lucienne-vorsatzpapier-0.1.0.css ../../assets/scss/lucienne/lucienne-0.1.0.css

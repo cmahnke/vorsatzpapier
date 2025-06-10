21df5a93909a4031acd298ab268ef7be2ad24d75
Christian Mahnke <cmahnke@gmail.com>
2025-06-10 15:46:53 +0200

    Fix target file name

--- Source Files/Mocks/scripts/lucienne.sh
+++ Source Files/Mocks/scripts/lucienne.sh
@@ -4,4 +4,4 @@ npm i
 npm run bundle
 
 cp build/dist/lucienne-0.1.0.js ../../assets/js/lucienne/
-cp build/dist/lucienne-vorsatzpapier-0.1.0.css ../../assets/scss/lucienne/
+cp build/dist/lucienne-vorsatzpapier-0.1.0.css ../../assets/scss/lucienne/lucienne-0.1.0.css

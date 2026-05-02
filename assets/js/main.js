f19146359806e0373b8afa5999e5aec9d7850a0f
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-05-02 06:43:59 +0200

    Try to fix build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,2 @@
-//import $ from 'jquery';
-//window.$ = window.jQuery = $;
-//window.jQuery.migrateMute = true;
-//export { $ }
-//import 'jquery-migrate';
-//import './production';
 import './iiif-image-viewer';
 import './functions';
-//window.$ = window.jQuery = require("jquery");
-//window.jQuery.migrateMute = true;
-//require("jquery-migrate");
-//require("./production");
-//require("./iiif-image-viewer");

e293c8b32d5317daf10ced82b18e6d3decbfe8aa
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-05-08 08:45:00 +0200

    Prepare migration to npm moduke

--- Source Files/Mocks/src/@types/assets.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.scss" {
-  const content: Record<string, string>;
-  export default content;
-}
-
-declare module "*/translations.json" {
-  const content: Record<string, Record<string, Record<string, string>>>;
-  export default content;
-}
